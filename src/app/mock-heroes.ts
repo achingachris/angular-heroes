6cd952894176e044629dd5e860b5b17fa7d65a65
doghot <achinga.chris@gmail.com>
2023-05-25 16:30:59 +0300

    lesson2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Captain Hilarious' },
+  { id: 13, name: 'Laugh Bomb' },
+  { id: 14, name: 'Speedy Jokester' },
+  { id: 15, name: 'Magnetized Clown' },
+  { id: 16, name: 'Elastic Comedian' },
+  { id: 17, name: 'Dynamite Prankster' },
+  { id: 18, name: 'Dr. LOL' },
+  { id: 19, name: 'Magma Giggles' },
+  { id: 20, name: 'Whirlwind of Laughter' },
+];
